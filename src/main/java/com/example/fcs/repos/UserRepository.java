47f85b4ab95bd01ac04ec490071ed02486a8ac21
Emmanuel <emmanuelabwao@gmail.com>
2025-05-14 11:03:38 +0300

    recurring payments enabled on DEV-14

--- src/main/java/com/example/fcs/repos/UserRepository.java
+++ src/main/java/com/example/fcs/repos/UserRepository.java
@@ -6,4 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<Users, Integer>{
 }
 
-//schedule recurring paymennts on mobile
+//schedule recurring paymennts on mobile version
