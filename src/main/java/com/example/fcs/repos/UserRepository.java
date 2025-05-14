eb358b5ea7b15579e257b6e5ee8d3fc31d2025f7
Emmanuel <emmanuelabwao@gmail.com>
2025-05-14 13:34:13 +0300

    DEV-27 transaction history enabled

--- src/main/java/com/example/fcs/repos/UserRepository.java
+++ src/main/java/com/example/fcs/repos/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.fcs.repos;
 
-import com.example.fcs.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.fcs.entities.Users;
 
 public interface UserRepository extends JpaRepository<Users, Integer>{
 }
 
+//transaction history
